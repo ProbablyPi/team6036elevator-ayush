aafa3d92e9b3ceef241941a2d5362e5635e27614
Ashray._.g <ashray.gupta@gmail.com>
2022-09-10 20:45:00 -0700

    remove old:

--- src/main/java/com/peninsula/frc2022/config/PortConstants.java
+++ src/main/java/com/peninsula/frc2022/config/PortConstants.java
@@ -34,41 +34,9 @@ public class PortConstants extends ConfigBase {
 		public static final int kE = 12;
 	}
 
-	/** Intake */
-	public static int kIntakeId = 31;
-
-	/** Indexer */
-	public static int kIntakeIndexerId = 36;
-
-	public static int kKickerId = 12;
-	public static int kStopperSensorId = 2;
-	public static int kIntakeIndexerSensorId = 0;
-	public static int kFirstPosSensorId = 1;
-
-	/** Shooter */
-	public static int kShooterMaster = 21;
-
-	public static int kShooterSlave = 22;
-	public static int kShooterHood = 23;
-
-	/** Climber * */
-	public static int kClimberRightTilter = 51;
-	public static int kClimberRightPuller = 55;
-//	public static int kClimberRightTilterEncoder = 6;
-//	public static int kClimberRightPullerEncoder = 4;
-
-	public static int kClimberLeftTilter = 53;
-	public static int kClimberLeftPuller = 58;
-//	public static int kClimberLeftTilterEncoder = 7;
-//	public static int kClimberLeftPullerEncoder = 3;
-
-	public static int kClimberLeftPullerStop = 4;
-	public static int kClimberRightPullerStop = 7;
 
 	/** Joysticks */
 	public static int kDriverId = 0;
 	public static int kOperatorId = 1;
 
-	/** LED Strip */
-	public static int kLighting = 0;
 }
